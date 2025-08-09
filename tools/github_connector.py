a70b820b549e7696d0350e1b417b92c5ac5b1993
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-08-09 18:49:56 -0300

    Update github_connector.py

--- tools/github_connector.py
+++ tools/github_connector.py
@@ -1,9 +1,23 @@
+import os
 from github import Github
 from github.Auth import Token
-from google.colab import userdata
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
 
 def connection(repositorio: str):
-    GITHUB_TOKEN = userdata.get('github_token')
-    auth = Token(GITHUB_TOKEN)
+    # Obtém o URL do Key Vault das variáveis de ambiente
+    key_vault_url = os.environ["KEY_VAULT_URL"]
+
+    # Usa a identidade gerenciada para autenticar de forma automática
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=key_vault_url, credential=credential)
+
+    # Obtém o token do GitHub do Key Vault.
+    # O nome do segredo deve ser o mesmo que você configurou no Key Vault
+    github_token = client.get_secret("githubapi").value
+
+    auth = Token(github_token)
     g = Github(auth=auth)
+    return g.get_repo(repositorio)
     return g.get_repo(repositorio)
