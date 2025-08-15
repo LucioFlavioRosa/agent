cb5bfb2d1f5d86788484e8d3657e2df1312af72e
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-08-15 16:13:35 -0300

    Update mcp_server_fastapi.py

--- mcp_server_fastapi.py
+++ mcp_server_fastapi.py
@@ -28,7 +28,7 @@ class StartAnalysisPayload(BaseModel):
     repo_name: str
     analysis_type: Literal["relatorio_cleancode", "relatorio_performance_eficiencia",
                            "relatorio_simplicacao_debito_tecnico", "relatorio_solid",
-                            "relatorio_teste_unitario"]
+                            "relatorio_teste_unitario", "relatorio_owasp"]
     branch_name: Optional[str] = None
     instrucoes_extras: Optional[str] = None
     usar_rag: bool = Field(False)
@@ -363,6 +363,7 @@ def get_status(job_id: str = Path(..., title="O ID do Job a ser verificado")):
         print(f"ERRO CRÍTICO de Validação no Job ID {job_id}: {e}")
         print(f"Dados brutos do job que causaram o erro: {job}")
         raise HTTPException(status_code=500, detail="Erro interno ao formatar a resposta do status do job.")
+
 
 
 
