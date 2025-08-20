b3ac7dc628ccc591141120de4019dd0bd9c70cce
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-08-19 22:32:28 -0300

    Update llm_provider_interface.py

--- domain/interfaces/llm_provider_interface.py
+++ domain/interfaces/llm_provider_interface.py
@@ -6,14 +6,14 @@ class ILLMProvider(ABC):
     Interface para provedores de Large Language Models (LLM).
     """
     @abstractmethod
-    def executar_analise_llm(
+    def executar_prompt(
         self,
-        tipo_analise: str,
-        codigo: str,
-        analise_extra: str,
+        tipo_tarefa: str,          
+        prompt_principal: str,   
+        instrucoes_extras: str,
         usar_rag: bool,
         model_name: str,
         max_token_out: int
     ) -> Dict[str, Any]:
-        """Executa a análise LLM e retorna o resultado no formato esperado."""
+        """Executa uma tarefa genérica no LLM e retorna o resultado."""
         pass
