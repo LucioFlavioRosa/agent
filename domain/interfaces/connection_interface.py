4f13b97e329c48f9a3ff4666caabc2ff2bd2eadb
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-16 20:16:04 -0300

    Delete domain/interfaces/connection_interface.py

--- domain/interfaces/connection_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-class IConnection(ABC):
-    @abstractmethod
-    def connection(self, repositorio: str, repository_type: str, repository_provider: Any) -> Any:
-        pass
