f7284f4accdcf8cb75125faea30dfab227b47392
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-16 17:22:34 -0300

    Delete domain/interfaces/changeset_filler_interface.py

--- domain/interfaces/changeset_filler_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Dict, Any
-
-class IChangesetFiller(ABC):
-    @abstractmethod
-    def main(self, json_agrupado: Dict[str, Any], json_inicial: Dict[str, Any]) -> Dict[str, Any]:
-        pass
