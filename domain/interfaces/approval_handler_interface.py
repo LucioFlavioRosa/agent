28e06d6cd1b22a7a7a89669e4af5442e4c7fe621
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-16 17:22:20 -0300

    Delete domain/interfaces/approval_handler_interface.py

--- domain/interfaces/approval_handler_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Dict, Any
-
-class IApprovalHandler(ABC):
-    @abstractmethod
-    def handle_approval_step(self, job_id: str, job_info: Dict[str, Any], step_index: int, step_result: Dict[str, Any]) -> None:
-        pass
