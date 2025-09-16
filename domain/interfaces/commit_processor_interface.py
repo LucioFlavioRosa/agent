84efaa542f6eb899241f6fb3b191076789c5ba2d
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-16 20:15:45 -0300

    Delete domain/interfaces/commit_processor_interface.py

--- domain/interfaces/commit_processor_interface.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Dict, Any, List
-
-class ICommitProcessor(ABC):
-    @abstractmethod
-    def process_commits(self, repo: Any, dados_finais_formatados: Dict[str, Any], 
-                      branch_base_para_pr: str, repository_type: str) -> List[Dict[str, Any]]:
-        pass
