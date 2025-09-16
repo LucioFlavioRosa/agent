984f409022550a9dae7a6c4aee457e065a302833
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-16 18:28:26 -0300

    Update dependency_container.py

--- services/dependency_container.py
+++ services/dependency_container.py
@@ -39,22 +39,6 @@ class DependencyContainer:
             self._workflow_registry_service = WorkflowRegistryService()
         return self._workflow_registry_service
     
-    def get_workflow_orchestrator(self):
-    if self._workflow_orchestrator is None:
-        workflow_registry = self.get_workflow_registry_service().get_workflow_registry()
-        self._workflow_orchestrator = WorkflowOrchestrator(
-            job_manager=self.get_job_manager(),
-            blob_storage=self.get_blob_storage(),
-            workflow_registry=workflow_registry,
-            rag_retriever=self.get_rag_retriever(),
-            changeset_filler=self.get_changeset_filler(),
-            reader=self.get_reader(),
-            repository_provider=self.get_repository_provider(),
-            connection=self.get_connection(),
-            commit_processor=self.get_commit_processor()
-        )
-    return self._workflow_orchestrator
-    
     def get_analysis_name_service(self) -> AnalysisNameService:
         if self._analysis_name_service is None:
             cache = AnalysisNameCache(self.get_job_store())
@@ -70,3 +54,19 @@ class DependencyContainer:
         if self._changeset_filler is None:
             self._changeset_filler = ChangesetFiller()
         return self._changeset_filler
+
+    def get_workflow_orchestrator(self):
+        if self._workflow_orchestrator is None:
+            workflow_registry = self.get_workflow_registry_service().get_workflow_registry()
+            self._workflow_orchestrator = WorkflowOrchestrator(
+                job_manager=self.get_job_manager(),
+                blob_storage=self.get_blob_storage(),
+                workflow_registry=workflow_registry,
+                rag_retriever=self.get_rag_retriever(),
+                changeset_filler=self.get_changeset_filler(),
+                reader=self.get_reader(),
+                repository_provider=self.get_repository_provider(),
+                connection=self.get_connection(),
+                commit_processor=self.get_commit_processor()
+            )
+        return self._workflow_orchestrator
