7050ea12b2626b4c51f6d211268f227b63b7674b
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-22 19:46:04 -0300

    refactor: services/step_executors/comparador_step_executor.py

--- services/step_executors/comparador_step_executor.py
+++ services/step_executors/comparador_step_executor.py
@@ -25,10 +25,7 @@ class ComparadorStepExecutor(BaseStepExecutor):
 
         agent_params['instrucoes_extras'] = instrucoes_formatadas
         agent_params.update({
-            'repositorio_modernizado': job_info['data'].get('repo_name_modernizado'),
-            'branch_modernizado': job_info['data'].get('branch_name_modernizado'),
-            'repositorio_original': job_info['data'].get('repo_name_original'),
-            'branch_original': job_info['data'].get('branch_name_original'),
+            'arquivos_especificos': job_info['data'].get('arquivos_especificos'),
             'repository_type': job_info['data']['repository_type'],
             'job_id': job_id,
             'projeto': job_info['data']['projeto'],
@@ -39,7 +36,7 @@ class ComparadorStepExecutor(BaseStepExecutor):
         agent_response = agente.main(**agent_params)
         
         json_string = agent_response.get('resultado', {}).get('reposta_final', {}).get('reposta_final', '')
-        cleaned_string = json_string.replace("```json", "").replace("```", "").strip()
+        cleaned_string = json_string.replace("", "").replace("", "").strip()
         
         if not cleaned_string:
             if previous_step_result and isinstance(previous_step_result, dict):
