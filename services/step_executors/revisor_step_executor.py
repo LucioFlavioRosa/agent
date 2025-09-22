9269df0ad372a7fc922cf82c82a1cea1530a459f
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-22 19:45:59 -0300

    refactor: services/step_executors/revisor_step_executor.py

--- services/step_executors/revisor_step_executor.py
+++ services/step_executors/revisor_step_executor.py
@@ -24,10 +24,13 @@ class RevisorStepExecutor(BaseStepExecutor):
             self.job_handler.update_job(job_id, job_info)
 
         agent_params['instrucoes_extras'] = instrucoes_formatadas
+        
+        if 'repositorio' not in agent_params:
+            agent_params['repositorio'] = job_info['data']['repo_name']
+        if 'nome_branch' not in agent_params:
+            agent_params['nome_branch'] = job_info['data']['branch_name']
+        
         agent_params.update({
-            'repositorio': job_info['data']['repo_name'],
-            'nome_branch': job_info['data']['branch_name'], 
-            'instrucoes_extras': instrucoes_formatadas,
             'arquivos_especificos': job_info['data'].get('arquivos_especificos'),
             'repository_type': job_info['data']['repository_type'],
             'job_id': job_id,
@@ -39,7 +42,7 @@ class RevisorStepExecutor(BaseStepExecutor):
         agent_response = agente.main(**agent_params)
         
         json_string = agent_response.get('resultado', {}).get('reposta_final', {}).get('reposta_final', '')
-        cleaned_string = json_string.replace("```json", "").replace("```", "").strip()
+        cleaned_string = json_string.replace("", "").replace("", "").strip()
         
         if not cleaned_string:
             if previous_step_result and isinstance(previous_step_result, dict):
