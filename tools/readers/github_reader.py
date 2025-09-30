80f7d2e7e2be35aea085021bffe2ed6e794962c7
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-09-30 17:34:55 -0300

    refactor: tools/readers/github_reader.py

--- tools/readers/github_reader.py
+++ tools/readers/github_reader.py
@@ -95,7 +95,7 @@ class GitHubReader(BaseReader):
         nome_branch: str = None,
         arquivos_especificos: Optional[List[str]] = None,
         mapeamento_tipo_extensoes: Dict = None,
-        incluir_lista_arquivos: bool = False
+        retornar_lista_arquivos: bool = False
     ) -> Union[Dict[str, str], Dict[str, Union[Dict[str, str], List[str]]]]:
         branch_a_ler = self._validar_parametros_leitura(repositorio, nome_branch, "GitHub")
         
@@ -107,11 +107,11 @@ class GitHubReader(BaseReader):
             extensoes_alvo = self._validar_extensoes_alvo(tipo_analise, mapeamento_tipo_extensoes)
             arquivos_lidos = self._ler_repositorio_completo(repositorio, branch_a_ler, tipo_analise, extensoes_alvo)
         
-        if incluir_lista_arquivos:
-            print("Flag incluir_lista_arquivos ativada - obtendo lista completa de arquivos GitHub.")
+        if retornar_lista_arquivos:
+            print("Flag retornar_lista_arquivos ativada - obtendo lista completa de arquivos GitHub.")
             lista_todos_arquivos = self._obter_lista_todos_arquivos(repositorio, branch_a_ler)
             return {
-                'arquivos': arquivos_lidos,
+                'codigo': arquivos_lidos,
                 'lista_arquivos': lista_todos_arquivos
             }
         else:
