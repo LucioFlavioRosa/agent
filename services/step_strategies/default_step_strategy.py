21ec530a85a70556f2ad9e2c65e0e83fb38224be
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-05 23:13:22 -0300

    Update default_step_strategy.py

--- services/step_strategies/default_step_strategy.py
+++ services/step_strategies/default_step_strategy.py
@@ -1,6 +1,9 @@
 from typing import Dict, Any
 from models import JobFields
 
+from services.step_executors.step_executor_factory import StepExecutorFactory
+from tools.readers.reader_geral import ReaderGeral
+
 class DefaultStepStrategy:
     def __init__(self, job_handler):
         self.job_handler = job_handler
