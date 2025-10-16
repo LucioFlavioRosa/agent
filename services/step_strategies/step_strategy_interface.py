ad8ee1191aeb68f1f19b6b75a68219b0bc7768cd
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-16 00:33:20 -0300

    refactor: services/step_strategies/step_strategy_interface.py

--- services/step_strategies/step_strategy_interface.py
+++ services/step_strategies/step_strategy_interface.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 from tools.readers.reader_geral import ReaderGeral
 
 class IStepStrategy(ABC):
     @abstractmethod
     def execute_step(self, job_id: str, job_info: Dict[str, Any], step: Dict[str, Any], 
                     current_step_index: int, previous_step_result: Dict[str, Any], 
-                    repo_reader: ReaderGeral, llm_provider, agent_params: Dict[str, Any]) -> Dict[str, Any]:
+                    repo_reader: ReaderGeral, llm_provider, agent_params: Dict[str, Any],
+                    access_token: str, access_token_original: Optional[str] = None) -> Dict[str, Any]:
         pass
     
     @abstractmethod
