fb9efa4b9d6c3b9ecf2b7027d476e1083afe2097
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-15 21:41:53 -0300

    limpeza

--- tools/readers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .reader_geral import ReaderGeral
-from .github_reader import GitHubReader
-from .gitlab_reader import GitLabReader
-from .azure_reader import AzureReader
-
-__all__ = ['ReaderGeral', 'GitHubReader', 'GitLabReader', 'AzureReader']
