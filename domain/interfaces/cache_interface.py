365cf408c83d949e76ed210c7ab601ba09c7ceb5
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-16 08:21:00 -0300

    feat: domain/interfaces/cache_interface.py

---
+++ domain/interfaces/cache_interface.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+
+class ICacheService(ABC):
+    @abstractmethod
+    def set(self, key: str, value: Any, ttl: Optional[int] = None):
+        pass
+
+    @abstractmethod
+    def get(self, key: str) -> Optional[Any]:
+        pass
+
+    @abstractmethod
+    def delete(self, key: str):
+        pass
+
+    @abstractmethod
+    def exists(self, key: str) -> bool:
+        pass
