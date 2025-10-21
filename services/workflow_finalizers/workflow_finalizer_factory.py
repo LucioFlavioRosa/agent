b20ba1869de86a9943b899c231ce179e35ae4035
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-21 15:51:13 -0300

    refactor: services/workflow_finalizers/workflow_finalizer_factory.py

--- services/workflow_finalizers/workflow_finalizer_factory.py
+++ services/workflow_finalizers/workflow_finalizer_factory.py
@@ -1,8 +1,9 @@
 from services.workflow_finalizers.commit_workflow_finalizer import CommitWorkflowFinalizer
 from services.workflow_finalizers.azure_devops_workflow_finalizer import AzureDevOpsWorkflowFinalizer
+from services.workflow_finalizers.epic_creation_workflow_finalizer import EpicCreationWorkflowFinalizer
 
 class WorkflowFinalizerFactory:
-    def __init__(self, data_formatter, job_handler, commit_handler, azure_devops_service, dotnet_build_service=None):
+    def __init__(self, data_formatter, job_handler, commit_handler, azure_devops_service, dotnet_build_service):
         self.data_formatter = data_formatter
         self.job_handler = job_handler
         self.commit_handler = commit_handler
@@ -11,14 +12,8 @@ class WorkflowFinalizerFactory:
 
     def get_finalizer(self, workflow_mode):
         if workflow_mode == 'epic_task_creation':
-            return AzureDevOpsWorkflowFinalizer(
-                azure_devops_service=self.azure_devops_service,
-                job_handler=self.job_handler
-            )
-        # default: code_generation
-        return CommitWorkflowFinalizer(
-            data_formatter=self.data_formatter,
-            job_handler=self.job_handler,
-            commit_handler=self.commit_handler,
-            dotnet_build_service=self.dotnet_build_service
-        )
+            return EpicCreationWorkflowFinalizer(self.azure_devops_service, self.job_handler)
+        elif workflow_mode == 'azure_devops':
+            return AzureDevOpsWorkflowFinalizer(self.azure_devops_service, self.job_handler)
+        else:
+            return CommitWorkflowFinalizer(self.data_formatter, self.job_handler, self.commit_handler, self.dotnet_build_service)
