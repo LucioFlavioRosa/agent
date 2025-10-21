062d637084dc6813e5dcd1c1cf2bc83395c4dd9d
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-21 11:31:41 -0300

    limpeza

--- domain/interfaces/llm_provider_factory_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-class ILLMProviderFactory(ABC):
-    @abstractmethod
-    def create_provider(self, model_name: str, rag_retriever: Any) -> Any:
-        pass
--- domain/interfaces/repository_provider_factory_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-class IRepositoryProviderFactory(ABC):
-    @abstractmethod
-    def get_provider(self, repository_type: str) -> Any:
-        pass
