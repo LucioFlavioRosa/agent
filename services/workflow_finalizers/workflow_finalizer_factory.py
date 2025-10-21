de8f3d44810f70f39c4cd21d8a2ba597bc947d9f
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-21 13:46:38 -0300

    feat: services/workflow_finalizers/workflow_finalizer_factory.py

---
+++ services/workflow_finalizers/workflow_finalizer_factory.py
@@ -0,0 +1,15 @@
+from services.workflow_finalizers.commit_workflow_finalizer import CommitWorkflowFinalizer
+from services.workflow_finalizers.azure_devops_workflow_finalizer import AzureDevOpsWorkflowFinalizer
+
+class WorkflowFinalizerFactory:
+    def __init__(self, data_formatter, job_handler, commit_handler, azure_devops_service, dotnet_build_service=None):
+        self.data_formatter = data_formatter
+        self.job_handler = job_handler
+        self.commit_handler = commit_handler
+        self.azure_devops_service = azure_devops_service
+        self.dotnet_build_service = dotnet_build_service
+
+    def get_finalizer(self, workflow_mode):
+        if workflow_mode == 'epic_task_creation':
+            return AzureDevOpsWorkflowFinalizer(self.azure_devops_service, self.job_handler)
+        return CommitWorkflowFinalizer(self.data_formatter, self.job_handler, self.commit_handler, self.dotnet_build_service)
