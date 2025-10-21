3bbcf32d62a44b3ecd049e8a0b60aac121de2bdc
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-21 17:05:39 -0300

    refactor: services/job_data_service.py

--- services/job_data_service.py
+++ services/job_data_service.py
@@ -28,7 +28,10 @@ class JobDataService:
         data[JobFields.ARQUIVOS_ESPECIFICOS] = payload_dict.get('arquivos_especificos')
         data[JobFields.REPOSITORY_TYPE] = payload_dict.get('repository_type')
         data[JobFields.REPO_NAME_MODERNIZADO] = payload_dict.get('repo_name_modernizado')
-        data[JobFields.BRANCH_NAME_MODERNIZADO] = payload_dict.get('branch_name_modernizado')
+        workflow_mode = payload_dict.get('workflow_mode')
+        # Passo 7: branch_name é condicional ao workflow_mode
+        if workflow_mode != 'epic_task_creation':
+            data[JobFields.BRANCH_NAME_MODERNIZADO] = payload_dict.get('branch_name_modernizado')
         data[JobFields.REPO_NAME_ORIGINAL] = payload_dict.get('repo_name_original')
         data[JobFields.BRANCH_NAME_ORIGINAL] = payload_dict.get('branch_name_original')
         data[JobFields.RETORNAR_LISTA_ARQUIVOS] = payload_dict.get('retornar_lista_arquivos', False)
