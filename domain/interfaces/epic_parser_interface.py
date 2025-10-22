5c7dbdb41a72fd5d08cfc6b9791e54e52258f1af
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-21 21:36:37 -0300

    limpeza

--- domain/interfaces/epic_parser_interface.py
+++
@@ -1,5 +0,0 @@
-from typing import List, Dict, Any
-
-class IEpicParser:
-    def parse_epic_table(self, markdown_table: str) -> List[Dict[str, Any]]:
-        raise NotImplementedError
--- services/workflow_finalizers/workflow_finalizer_interface.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class IWorkflowFinalizer(ABC):
-    @abstractmethod
-    def finalize(self, job_id, job_info, workflow, final_result, repository_type, repo_name):
-        pass
