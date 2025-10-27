5ee85810b891d59388b77050b18f4f00b0ea2c36
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-10-26 23:50:04 -0300

    Update job_data_service.py

--- services/job_data_service.py
+++ services/job_data_service.py
@@ -43,9 +43,9 @@ class JobDataService:
             epic_id = payload_dict.get('epic_id')
             organization, project = self._parse_repository_name(normalized_repo_name)
             data[JobFields.EPIC_ID] = epic_id
+            data[JobFields.TASK_ID] = task_id
             data[JobFields.ORGANIZATION] = organization
             data[JobFields.PROJECT] = project
-            print(f"[DEBUG] Propagando task_id no create_initial_job_data: {task_id}")
         data[JobFields.PROJETO] = payload_dict.get('projeto')
         data[JobFields.ANALYSIS_NAME] = analysis_name
         data[JobFields.ORIGINAL_ANALYSIS_TYPE] = payload_dict.get('analysis_type')
@@ -72,7 +72,6 @@ class JobDataService:
             criar_tarefas_azure = bool(criar_tarefas_azure)
         data[JobFields.CRIAR_TAREFAS_AZURE] = criar_tarefas_azure
         data['criar_tarefas_azure'] = criar_tarefas_azure
-        data[JobFields.TASK_ID] = payload_dict.get('task_id')
         return {
             JobFields.STATUS: None,
             JobFields.DATA: data
