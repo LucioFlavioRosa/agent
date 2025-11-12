c440c8ec05e7c59d2ac19709069cfe1a5c3b35d2
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-11-12 13:45:25 -0300

    Update azure_board_service.py

--- services/azure_board_service.py
+++ services/azure_board_service.py
@@ -290,6 +290,8 @@ class AzureBoardService:
                     'id': data.get('id'),
                     'title': fields.get('System.Title'),
                     'description': fields.get('System.Description'),
+                    'acceptance_criteria': fields.get('Microsoft.VSTS.Common.AcceptanceCriteria'),
+                    'state': fields.get('System.State'),
                     'state': fields.get('System.State'),
                     'url': data.get('url'),
                     'fields': fields
