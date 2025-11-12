38a85043fc4a5ee8367a8638823b083e7f6c1391
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-11-12 10:41:04 -0300

    Add feature_id and epic_id to job data

--- services/job_data_service.py
+++ services/job_data_service.py
@@ -37,8 +37,10 @@ class JobDataService:
             data[JobFields.PROJECT] = project
         if analysis_type == 'criacao_features_azure_devops':
             epic_id = payload_dict.get('epic_id')
+            feature_id = payload_dict.get('feature_id')
             organization, project = self._parse_repository_name(normalized_repo_name)
             data[JobFields.EPIC_ID] = epic_id
+            data[JobFields.FEATURE_ID] = feature_id
             data[JobFields.ORGANIZATION] = organization
             data[JobFields.PROJECT] = project
         # Passo 7: garantir task_id para revisor_tarefas
