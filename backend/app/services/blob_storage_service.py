fdfed95be2ca7823d73995d354543bf80ad2c205
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-12-04 01:21:30 -0300

    refactor: backend/app/services/blob_storage_service.py

--- backend/app/services/blob_storage_service.py
+++ backend/app/services/blob_storage_service.py
@@ -11,7 +11,6 @@ logger = logging.getLogger(__name__)
 _blob_service_client_singleton = None
 _container_client_singleton = None
 
-
 def _get_blob_clients():
     global _blob_service_client_singleton, _container_client_singleton
     connection_string = getattr(settings, "AZURE_STORAGE_CONNECTION_STRING", None)
