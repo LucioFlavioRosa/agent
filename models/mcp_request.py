0b6220da4efc1ff12429ad997f5d5168e403f9d0
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-12-10 23:19:30 -0300

    Add texto_extraido_do_docx field to MCPRequest model

--- models/mcp_request.py
+++ models/mcp_request.py
@@ -5,6 +5,6 @@ class MCPRequest(BaseModel):
     project_id: str
     analysis_type: str
     instrucoes_extras: Optional[str] = None
-    arquivo_docx: Optional[str] = None
     nome_projeto: Optional[str] = None
     usuario_executor: Optional[str] = None
+    texto_extraido_do_docx: Optional[str] = None
