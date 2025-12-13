c2bf10e8eb8e62c180a9dd8253ef8888743fc547
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-12-13 19:00:17 -0300

    Update project_models.py

--- backend/app/models/project_models.py
+++ backend/app/models/project_models.py
@@ -8,3 +8,4 @@ class ProjectListItem(BaseModel):
     created_at: Optional[datetime] = Field(None)
     last_saved_to_blob: Optional[datetime] = Field(None)
     project_id: Optional[str] = Field(default=None)
+    job_id: Optional[str] = Field(default=None)
