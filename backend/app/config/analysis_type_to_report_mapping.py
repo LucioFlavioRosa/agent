27d13ff6767bc4087c016e5d6990da60dc8193f6
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2025-12-14 23:48:40 -0300

    Update analysis_type_to_report_mapping.py

--- backend/app/config/analysis_type_to_report_mapping.py
+++ backend/app/config/analysis_type_to_report_mapping.py
@@ -2,12 +2,11 @@ analysis_type_to_report_mapping = {
     "criacao_epicos_azure_devops": "epicos_report",
     "refinamento_epicos_azure_devops": "epicos_report",
     "criacao_epicos_timeline": "epicos_timeline_report",
+    "refinamento_epicos_timeline": "epicos_timeline_report",
     "criacao_features_azure_devops": "features_report",
     "refinamento_features_azure_devops": "features_report",
-    "criacao_times_azure_devops": "times_descricao_report",
-    "refinamento_times_azure_devops": "times_descricao_report",
     "criacao_alocacao_azure_devops": "alocacao_times_report",
     "refinamento_alocacao_azure_devops": "alocacao_times_report",
-    "criacao_premissas_azure_devops": "premissas_riscos_report",
-    "refinamento_premissas_azure_devops": "premissas_riscos_report"
+    "criacao_premissas_azure": "premissas_riscos_report",
+    "refinamento_premissas_azure": "premissas_riscos_report"
 }
