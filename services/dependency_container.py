89c4e0bc14f74fe4bf24b7df7c76d4f2a609de99
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-02-03 09:35:39 -0300

    refactor: services/dependency_container.py

--- services/dependency_container.py
+++ services/dependency_container.py
@@ -8,6 +8,8 @@ from services.job_handler import JobHandler
 from services.report_handler import ReportHandler
 from services.redis_cache_service import RedisCacheService
 from tools.azure_secret_manager import AzureSecretManager
+# Importa o resolver de grupo do MongoDB
+from services.mongodb_group_resolver_service import MongoDBGroupResolverService
 
 class DependencyContainer:
     def __init__(self):
@@ -21,6 +23,7 @@ class DependencyContainer:
         self._report_handler = None
         self._redis_cache_service = None
         self._secret_manager = None
+        self._mongodb_group_resolver = None
     
     def get_job_store(self) -> RedisJobStore:
         if self._job_store is None:
@@ -32,10 +35,16 @@ class DependencyContainer:
             self._job_manager = JobManager(self.get_job_store())
         return self._job_manager
     
+    def get_mongodb_group_resolver(self) -> MongoDBGroupResolverService:
+        if self._mongodb_group_resolver is None:
+            self._mongodb_group_resolver = MongoDBGroupResolverService()
+        return self._mongodb_group_resolver
+    
     def get_blob_storage(self, user_email: str = None) -> BlobStorageService:
         key = user_email or '__default__'
         if key not in self._blob_storage_instances:
-            self._blob_storage_instances[key] = BlobStorageService(user_email=user_email)
+            group_resolver = self.get_mongodb_group_resolver()
+            self._blob_storage_instances[key] = BlobStorageService(user_email=user_email, group_resolver=group_resolver)
         return self._blob_storage_instances[key]
     
     def get_workflow_registry_service(self) -> WorkflowRegistryService:
@@ -51,7 +60,8 @@ class DependencyContainer:
     def get_report_handler(self, user_email: str = None) -> ReportHandler:
         if self._report_handler is None:
             blob_storage = self.get_blob_storage(user_email)
-            self._report_handler = ReportHandler(blob_storage)
+            group_resolver = self.get_mongodb_group_resolver()
+            self._report_handler = ReportHandler(blob_storage, group_resolver=group_resolver)
         return self._report_handler
     
     def get_secret_manager(self) -> AzureSecretManager:
@@ -68,6 +78,7 @@ class DependencyContainer:
     def get_workflow_orchestrator(self, user_email: str = None) -> WorkflowOrchestrator:
         if self._workflow_orchestrator is None:
             workflow_registry = self.get_workflow_registry_service().get_workflow_registry()
+            group_resolver = self.get_mongodb_group_resolver()
             self._workflow_orchestrator = WorkflowOrchestrator(
                 job_manager=self.get_job_manager(), 
                 blob_storage=self.get_blob_storage(user_email), 
@@ -76,7 +87,8 @@ class DependencyContainer:
                 report_handler=self.get_report_handler(user_email),
                 secret_manager=self.get_secret_manager(),
                 cache_service=self.get_redis_cache_service(),
-                dependency_container=self
+                dependency_container=self,
+                group_resolver=group_resolver
             )
         return self._workflow_orchestrator
     
