f784bc99b5eb71758893e45533eeab72926d2e9c
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-02-03 09:35:51 -0300

    refactor: tools/blob_report_reader.py

--- tools/blob_report_reader.py
+++ tools/blob_report_reader.py
@@ -4,12 +4,12 @@ from tools.azure_secret_manager import AzureSecretManager
 from tools.blob_report_path_builder import build_report_blob_path
 from tools.blob_storage_utils import get_blob_connection_string
 
-def read_report_from_blob(projeto: str, analysis_type: str, repository_type: str, repo_name: str, branch_name: str, analysis_name: str, user_email: str) -> str:
+def read_report_from_blob(projeto: str, analysis_type: str, repository_type: str, repo_name: str, branch_name: str, analysis_name: str, user_email: str, group_resolver: object = None) -> str:
     container_name = os.getenv('AZURE_STORAGE_CONTAINER_NAME')
     if not container_name:
         raise RuntimeError('Azure Blob Storage container name missing.')
     secret_manager = AzureSecretManager()
-    connection_string = get_blob_connection_string(secret_manager, user_email)
+    connection_string = get_blob_connection_string(secret_manager, user_email, group_resolver)
     blob_path = build_report_blob_path(projeto, analysis_type, repository_type, repo_name, branch_name, analysis_name)
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_path)
