cdaf86d770fc91593bf6513c03eb8922b7ff62ae
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-02-02 21:28:29 -0300

    refactor: services/factories/llm_provider_factory.py

--- services/factories/llm_provider_factory.py
+++ services/factories/llm_provider_factory.py
@@ -1,10 +1,7 @@
 from tools.requisicao_claude import AmazonBedrockProvider
 from services.azure_secret_manager import AzureSecretManager, VaultType
 
-def create_provider(model_name=None, rag_retriever=None, secret_manager=None):
-    # O secret_manager pode ser passado ou será criado com vault_type LLM
+def create_provider(model_name=None, secret_manager=None):
     secret_manager = secret_manager or AzureSecretManager(vault_type=VaultType.LLM)
-    # Instancia o AmazonBedrockProvider
     provider = AmazonBedrockProvider(secret_manager=secret_manager)
-    # O rag_retriever não é utilizado diretamente no BedrockProvider, mas pode ser passado para futuras extensões
     return provider
