23fa03ac93cf78450fb2122510392a8536f948d2
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-02-10 16:53:52 -0300

    refactor: remove backend/app/models/azure_ad_models.py

--- backend/app/models/azure_ad_models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional, List
-
-class AzureADTokenData(BaseModel):
-    oid: str = Field(..., description="Object ID do usuário no Azure AD.")
-    preferred_username: Optional[str] = Field(None, description="Nome de usuário preferencial do Azure AD.")
-    email: Optional[str] = Field(None, description="Email do usuário.")
-    roles: Optional[List[str]] = Field(default_factory=list, description="Lista de roles do usuário.")
-    name: Optional[str] = Field(None, description="Nome completo do usuário.")
