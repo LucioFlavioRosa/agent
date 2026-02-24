040d9c126aa1f4223ec21002452ad7824f596ec9
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-02-24 20:50:35 -0300

    Update agent_mapping.py

--- backend/app/config/agent_mapping.py
+++ backend/app/config/agent_mapping.py
@@ -2,19 +2,19 @@ AGENT_CONFIG = {
     "agent_epics_generator_digital": {
         "prompt_file": "create_epics.md",
         "output_filename": "epics.md",      
-        "llm_model": "gpt-4o",
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
         "service": "claude_aws_service"
     },
     "agent_features_generator_digital": {
         "prompt_file": "create_features.md",
         "output_filename": "features.md",
-        "llm_model": "gpt-4o-mini",
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
         "service": "claude_aws_service"
     },
     "agent_timeline_generator_digital": {
         "prompt_file": "create_timeline.md",
         "output_filename": "timeline.md",
-        "llm_model": "gpt-4o",
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
         "service": "claude_aws_service"
     }
 }
