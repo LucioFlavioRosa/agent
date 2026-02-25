1e8bea4b01c9886c92fb032728cb81c7b64c1528
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-02-25 10:51:04 -0300

    Update agent_mapping.py

--- backend/app/config/agent_mapping.py
+++ backend/app/config/agent_mapping.py
@@ -5,6 +5,12 @@ AGENT_CONFIG = {
         "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
         "service": "claude_aws_service"
     },
+    "agent_epics_reviwer_digital": {
+        "prompt_file": "review_epics.md",
+        "output_filename": "epics.md",      
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+        "service": "claude_aws_service"
+    },
     "agent_features_generator_digital": {
         "prompt_file": "create_features.md",
         "output_filename": "features.md",
