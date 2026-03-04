ededb7a4a0b60be732fd7bd2da43119b109cbeae
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-03-03 22:57:21 -0300

    Update agent_mapping.py

--- backend/app/config/agent_mapping.py
+++ backend/app/config/agent_mapping.py
@@ -28,5 +28,11 @@ AGENT_CONFIG = {
         "output_filename": "timeline.md",
         "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
         "service": "claude_aws_service"
+    },
+    "agent_timeline_reviwer_digital": {
+        "prompt_file": "review_timeline.md",
+        "output_filename": "timeline.md",
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+        "service": "claude_aws_service"
     }
 }
