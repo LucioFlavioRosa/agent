ad4c3f825b291d488bad379820f0e7cda580bb44
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-03-06 20:23:50 -0300

    Create agent_mapping.py

---
+++ app/config/agent_mapping.py
@@ -0,0 +1,14 @@
+AGENT_CONFIG = {
+    "agent_prototipo_generator_digital": {
+        "prompt_file": "create_prototipo.md",
+        "output_filename": "index.html",      
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+        "service": "claude_aws_service"
+    },
+    "agent_epics_reviwer_digital": {
+        "prompt_file": "review_prototipo.md",
+        "output_filename": "index.html",      
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+        "service": "claude_aws_service"
+    }
+}
