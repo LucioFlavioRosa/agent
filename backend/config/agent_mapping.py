a17f50ab5c93a996788d5e48760aaf8e3e43c518
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-03-16 18:54:07 -0300

    Update agent_mapping.py

--- backend/config/agent_mapping.py
+++ backend/config/agent_mapping.py
@@ -8,5 +8,6 @@ AGENT_TO_CATEGORY = {
     "agent_risks_generator_digital": "risks",
     "agent_risks_reviwer_digital": "risks",
     "agent_prototype_generator_digital": "prototype",
-    "agent_prototype_reviwer_digital": "prototype"
+    "agent_prototype_reviwer_digital": "prototype",
+    "agent_prototype_fromepic_digital": "prototype"
 }
