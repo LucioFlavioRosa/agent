b155e60118088d610741bce9c0e7b40876ffc381
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-03-16 21:39:41 -0300

    Update agent_mapping.py

--- app/config/agent_mapping.py
+++ app/config/agent_mapping.py
@@ -12,5 +12,12 @@ AGENT_CONFIG = {
         "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
         "service": "bedrock_service", 
         "category": "prototype"       
+    },
+    "agent_prototype_fromepic_digital": {  
+        "prompt_file": "epic_prototipo.md",
+        "output_filename": "index.html",      
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+        "service": "bedrock_service", 
+        "category": "prototype"       
     }
 }
