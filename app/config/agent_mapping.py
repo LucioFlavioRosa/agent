ccb239d99d2a793924978c4634037a86f48f52b8
LucioFlavioRosa <45227658+LucioFlavioRosa@users.noreply.github.com>
2026-03-30 15:47:18 -0300

    Update agent_mapping.py

--- app/config/agent_mapping.py
+++ app/config/agent_mapping.py
@@ -19,5 +19,19 @@ AGENT_CONFIG = {
         "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
         "service": "bedrock_service", 
         "category": "prototype"       
+    },
+    "agent_prototype_generator_centerragold": {
+        "prompt_file": "create_prototype_centerragold.md",
+        "output_filename": "index.html",      
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+        "service": "bedrock_service", 
+        "category": "prototype"     
+    },
+    "agent_prototype_reviwer_centerragold": {  
+        "prompt_file": "review_prototype_centerragold.md",
+        "output_filename": "index.html",      
+        "llm_model": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
+        "service": "bedrock_service", 
+        "category": "prototype"       
     }
 }
